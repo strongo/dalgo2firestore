c3fec8fe950713994d6870bc886cb245db171a30
Alexander Trakhimenok <alex@trakhimenok.com>
2021-10-04 20:08:06 +0100

    implemented database.Update() & UpdateMulti()

--- transaction.go
+++ transaction.go
@@ -64,37 +64,6 @@ func (t transaction) Set(ctx context.Context, record dalgo.Record) error {
 	return t.tx.Set(dr, record.Data())
 }
 
-func (t transaction) Update(
-	_ context.Context,
-	key *dalgo.Key,
-	updates []dalgo.Update,
-	preconditions ...dalgo.Precondition,
-) error {
-	dr := t.dtb.doc(key)
-	fsUpdates := make([]firestore.Update, len(updates))
-	for i, u := range updates {
-		fsUpdates[i] = getFirestoreUpdate(u)
-	}
-	fsPreconditions := getUpdatePreconditions(preconditions)
-	return t.tx.Update(dr, fsUpdates, fsPreconditions...)
-}
-
-func getFirestoreUpdate(update dalgo.Update) firestore.Update {
-	return firestore.Update{
-		Path:      update.Field,
-		FieldPath: (firestore.FieldPath)(update.FieldPath),
-		Value:     update.Value,
-	}
-}
-
-func getUpdatePreconditions(preconditions []dalgo.Precondition) (fsPreconditions []firestore.Precondition) {
-	p := dalgo.GetPreconditions(preconditions...)
-	if p.Exists() {
-		fsPreconditions = []firestore.Precondition{firestore.Exists}
-	}
-	return fsPreconditions
-}
-
 func (t transaction) Delete(ctx context.Context, key *dalgo.Key) error {
 	dr := t.dtb.doc(key)
 	return t.tx.Delete(dr)
@@ -122,27 +91,6 @@ func (t transaction) GetMulti(ctx context.Context, records []dalgo.Record) error
 
 func (t transaction) SetMulti(ctx context.Context, records []dalgo.Record) error {
 	panic("implement me")
-}
-
-func (t transaction) UpdateMulti(
-	_ context.Context,
-	keys []*dalgo.Key,
-	updates []dalgo.Update,
-	preconditions ...dalgo.Precondition,
-) error {
-	fsPreconditions := getUpdatePreconditions(preconditions)
-	for _, key := range keys {
-		dr := t.dtb.doc(key)
-		fsUpdates := make([]firestore.Update, len(updates))
-		for i, u := range updates {
-			fsUpdates[i] = getFirestoreUpdate(u)
-		}
-		if err := t.tx.Update(dr, fsUpdates, fsPreconditions...); err != nil {
-			keyPath := PathFromKey(key)
-			return fmt.Errorf("failed to update record with key: %v: %w", keyPath, err)
-		}
-	}
-	return nil
 }
 
 func (t transaction) DeleteMulti(_ context.Context, keys []*dalgo.Key) error {
--- updater.go
+++ updater.go
@@ -1,7 +1,9 @@
 package dalgo2firestore
 
 import (
+	"cloud.google.com/go/firestore"
 	"context"
+	"fmt"
 	"github.com/strongo/dalgo"
 )
 
@@ -10,19 +12,75 @@ type updater struct {
 }
 
 func (u updater) Update(
-	_ context.Context,
-	_ *dalgo.Key,
-	_ []dalgo.Update,
-	_ ...dalgo.Precondition,
+	ctx context.Context,
+	key *dalgo.Key,
+	update []dalgo.Update,
+	preconditions ...dalgo.Precondition,
 ) error {
-	panic("not supported")
+	return u.dtb.RunInTransaction(ctx, func(ctx context.Context, d dalgo.Transaction) error {
+		return d.Update(ctx, key, update, preconditions...)
+	})
 }
 
 func (u updater) UpdateMulti(
-	_ context.Context,
-	_ []*dalgo.Key,
-	_ []dalgo.Update,
-	_ ...dalgo.Precondition,
+	ctx context.Context,
+	keys []*dalgo.Key,
+	updates []dalgo.Update,
+	preconditions ...dalgo.Precondition,
 ) error {
-	panic("not supported")
+	return u.dtb.RunInTransaction(ctx, func(ctx context.Context, d dalgo.Transaction) error {
+		return d.UpdateMulti(ctx, keys, updates, preconditions...)
+	})
+}
+
+func (t transaction) Update(
+	_ context.Context,
+	key *dalgo.Key,
+	updates []dalgo.Update,
+	preconditions ...dalgo.Precondition,
+) error {
+	dr := t.dtb.doc(key)
+	fsUpdates := make([]firestore.Update, len(updates))
+	for i, u := range updates {
+		fsUpdates[i] = getFirestoreUpdate(u)
+	}
+	fsPreconditions := getUpdatePreconditions(preconditions)
+	return t.tx.Update(dr, fsUpdates, fsPreconditions...)
+}
+
+func (t transaction) UpdateMulti(
+	_ context.Context,
+	keys []*dalgo.Key,
+	updates []dalgo.Update,
+	preconditions ...dalgo.Precondition,
+) error {
+	fsPreconditions := getUpdatePreconditions(preconditions)
+	for _, key := range keys {
+		dr := t.dtb.doc(key)
+		fsUpdates := make([]firestore.Update, len(updates))
+		for i, u := range updates {
+			fsUpdates[i] = getFirestoreUpdate(u)
+		}
+		if err := t.tx.Update(dr, fsUpdates, fsPreconditions...); err != nil {
+			keyPath := PathFromKey(key)
+			return fmt.Errorf("failed to update record with key: %v: %w", keyPath, err)
+		}
+	}
+	return nil
+}
+
+func getFirestoreUpdate(update dalgo.Update) firestore.Update {
+	return firestore.Update{
+		Path:      update.Field,
+		FieldPath: (firestore.FieldPath)(update.FieldPath),
+		Value:     update.Value,
+	}
+}
+
+func getUpdatePreconditions(preconditions []dalgo.Precondition) (fsPreconditions []firestore.Precondition) {
+	p := dalgo.GetPreconditions(preconditions...)
+	if p.Exists() {
+		fsPreconditions = []firestore.Precondition{firestore.Exists}
+	}
+	return fsPreconditions
 }
