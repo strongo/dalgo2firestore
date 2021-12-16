8b090c914b563229672c64f62348a856cdaf5037
Alexander Trakhimenok <alex@trakhimenok.com>
2021-12-16 23:24:06 +0000

    firestore emulator

--- e2e_test.go
+++ e2e_test.go
@@ -7,20 +7,31 @@ import (
 	"log"
 	"os"
 	"testing"
+	"time"
 )
 
 func TestEndToEnd(t *testing.T) {
 
+	setEnv := func(key, value string) {
+		if err := os.Setenv(key, value); err != nil {
+			t.Fatal(err)
+		}
+	}
+	setEnv("GCLOUD_PROJECT", "dalgo")
+	setEnv("FIREBASE_AUTH_EMULATOR_HOST", "localhost:9099")
+	setEnv("FIRESTORE_EMULATOR_HOST", "localhost:8080")
+
 	firestoreProjectID := os.Getenv("DALGO_E2E_PROJECT_ID")
 
 	if firestoreProjectID == "" {
-		firestoreProjectID = "DALGO_E2E"
+		firestoreProjectID = "dalgo"
 		//t.Fatalf("Environment variable DALGO_E2E_PROJECT_ID is not set")
 	}
 	log.Println("Firestore Project ID:", firestoreProjectID)
 	//log.Println("ENV: GOOGLE_APPLICATION_CREDENTIALS:", os.Getenv("GOOGLE_APPLICATION_CREDENTIALS"))
 
-	ctx := context.Background()
+	timeout, _ := time.ParseDuration("5s")
+	ctx, _ := context.WithDeadline(context.Background(), time.Now().Add(timeout))
 
 	var client *firestore.Client
 	var err error
